4ee3c33ece50472e8811aed256a841cc9ab8d2de
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-12 16:34:20 -0500

    Chapter 5... Variabls and Basic Expressions

---
+++ Chapter 5, Numeric Types/variablesAndBasicExpressions.py
@@ -0,0 +1,18 @@
+'''
+First of all, let’s exercise some basic math. In the following interaction, we first assign
+two variables (a and b) to integers so we can use them later in a larger expression.
+Variables are simply names—created by you or Python—that are used to keep track of
+information in your program. We’ll say more about this in the next chapter, but in
+Python:
+• Variables are created when they are first assigned values.
+• Variables are replaced with their values when used in expressions.
+• Variables must be assigned before they can be used in expressions.
+• Variables refer to objects and are never declared ahead of time.
+In other words, these assignments cause the variables a and b to spring into existence
+automatically:
+'''
+
+a = 3                   # Name created
+b = 4
+
+print(str(a) + " & " + str(b) + "\n")
