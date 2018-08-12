cb093fa76cb9f646d2dd2fbc25b7f6839f9ce594
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-08-11 19:32:08 -0500

    Changing the name of the directory structure, and adding a new Chapter.

---
+++ Chapter 3, How You Run Programs/script1.py
@@ -0,0 +1,11 @@
+'''
+Let’s get started. Open your favorite text editor (e.g., vi, Notepad, or the IDLE editor),
+and type the following statements into a new text file named script1.py:
+'''
+
+# A first Python script
+import sys              # Load a library module
+print(sys.platform)
+print(2 ** 100)         # Raise 2 to a power
+x = 'Spam!'
+print(x * 8)            # String repetition
